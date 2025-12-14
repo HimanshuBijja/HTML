52c8d91da5000e75e660d15a462a12aaf345e94c
Himanshu Bijja <bijjahimanshu05@gmail.com>
2025-12-14 18:50:17 +0530

    changes delay time

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ window.addEventListener("load", function () {
   for (let i = 1; i <= 30; i++) {
     const leaf = document.getElementById(`leaf-${i}`);
     if (leaf) {
-      const delay = (i - 1) * 100; // 100ms delay between each leaf
+      const delay = (i - 1) * 50; // 80ms delay between each leaf
 
       setTimeout(() => {
         leaf.style.transition = "opacity 0.4s ease-out";
